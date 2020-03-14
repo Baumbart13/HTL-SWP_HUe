b411b52527a1647167639846f920eaf354039783
Baumbart13 <46692947+Baumbart13@users.noreply.github.com>
2020-03-14 16:46:33 +0100

    Add files via upload

--- date2020_03_13/Page220.java
+++ date2020_03_13/Page220.java
@@ -1,9 +1,11 @@
 package date2020_03_13;
 
+import java.time.Month;
+
 public class Page220 {
     public static void main(String[] args) {
-        //System.out.println(Exercise1.foo());
-        //System.out.println(Exercise2.foo(2014, Month.MAY));
+        System.out.println(Exercise1.foo());
+        System.out.println(Exercise2.foo(2014, Month.MAY));
         Exercise3.foo();
     }
 }
